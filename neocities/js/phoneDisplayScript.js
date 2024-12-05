d2854f6c0a9914ddb39c28772b45730bcbf3ca15
Selkiefawn <136149232+Selkiefawn@users.noreply.github.com>
2024-12-04 21:50:02 -0500

    hopefully fully fix the bouncing phone problem.

--- neocities/js/phoneDisplayScript.js
+++ neocities/js/phoneDisplayScript.js
@@ -1,14 +1,11 @@
 let phone = document.getElementById("phone");
 let phoneSpot = document.getElementById("phoneSpot");
 let timeoutID = null;
-
+let mouseWithin = false; // ! Trying a flag to see if it fixes my problem.
 let time = 1000;
 
 function hidePhone() {
-    if (!phoneSpot.matches(':hover') && !phone.matches(':hover')) {
-        phone.classList.add('hidden');
-        phone.classList.remove('visible');
-    } else if (!phoneSpot.matches(':focus') && !phone.matches(':focus')) {
+    if (!mouseWithin) {
         phone.classList.add('hidden');
         phone.classList.remove('visible');
     }
@@ -16,27 +13,36 @@ function hidePhone() {
 
 phoneSpot.addEventListener('mouseenter', () => {
     clearTimeout(timeoutID);
+    mouseWithin = true; 
     phone.classList.remove('hidden');
     phone.classList.add('visible');
+    // console.log("Phone Visible!");
 });
 
 phoneSpot.addEventListener('mouseleave', () => {
+    mouseWithin = false;
     timeoutID = setTimeout(hidePhone, time);
+    // console.log("Phone Hidden!");
 });
 
 phone.addEventListener('mouseenter', () => {
     clearTimeout(timeoutID);
+    mouseWithin = true; 
+    // console.log("Keeping Open!");
 });
 
 phone.addEventListener('mouseleave', () => {
+    mouseWithin = false;
     timeoutID = setTimeout(hidePhone, time);
 });
 
 // Fallback just in case something breaks... it probably won't. 
+
 document.addEventListener('mousemove', (e) => {
     const windowWidth = window.innerWidth;
     if (e.clientX > windowWidth - 50 && phone.classList.contains('hidden')) {
         phone.classList.remove('hidden');
         phone.classList.add('visible');
+        // console.log("The cake is a lie!")
     }
 });
