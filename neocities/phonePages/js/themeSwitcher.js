d71f1acce6367c809e8cefbc72da6bd9125a35fd
Selkiefawn <136149232+Selkiefawn@users.noreply.github.com>
2024-12-27 11:28:01 -0500

    remove christmas theme and updated the theme switcher code a bit!

--- neocities/phonePages/js/themeSwitcher.js
+++ neocities/phonePages/js/themeSwitcher.js
@@ -1,16 +1,12 @@
 // ! First error: Check if you added IDs to the theme css links...
 
 document.addEventListener("DOMContentLoaded", function () {
+    localStorage.removeItem("forceChristmasTheme");
+
     const topPath = window.top.location.pathname.split('/').pop();
     const topPageName = topPath ? topPath.split('.')[0] : "index";
 
-    // ! You will be jolly.
-    const forceChristmasTheme = localStorage.getItem("forceChristmasTheme") || "true";
-
-    // ~ For top level html files only!
-
     const themeLinks = Array.from(window.top.document.querySelectorAll(`link[id^="${topPageName}"]`));
-
 
     if (!themeLinks.length) {
         console.warn(`No themes for ${topPageName} found. Skipping theme initialization!`);
@@ -22,21 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
         return themeName;
     });
 
-    // console.log('Available themes for this page:', availableThemes);
-
     const themeButtons = document.querySelectorAll("[data-theme]");
 
     function applyInitialTheme() {
-        // ? For the first time in forever...
-        if (forceChristmasTheme === "true") {
-            console.log("You will be jolly.");
-            setTheme("christmas");
-            localStorage.setItem("forceChristmasTheme", "false"); 
-        } else {
-            const savedTheme = localStorage.getItem("globalTheme") || "light";
-            const validTheme = availableThemes.includes(savedTheme) ? savedTheme : "light";
-            setTheme(validTheme);
-        }
+        const savedTheme = localStorage.getItem("globalTheme") || "light";
+        const validTheme = availableThemes.includes(savedTheme) ? savedTheme : "light";
+        setTheme(validTheme);
     }
 
     function applyTheme(theme) {
@@ -64,28 +51,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function setTheme(theme) {
-        // console.log("Applying theme:", theme);
-
         const themeId = theme === 'light'
             ? `${topPageName}CSS`
             : `${topPageName}${theme.charAt(0).toUpperCase() + theme.slice(1)}CSS`;
 
         themeLinks.forEach(link => {
             link.disabled = link.id !== themeId;
-            // console.log(`Link: ${link.id}, Disabled: ${link.disabled}`);
         });
 
         localStorage.setItem("globalTheme", theme);
         themeButtons.forEach(button => {
             button.disabled = button.dataset.theme === theme;
         });
-
-        // ? Nothing's in my way!
-        if (theme === "christmas") {
-            localStorage.setItem("forceChristmasTheme", "true");
-        } else {
-            localStorage.setItem("forceChristmasTheme", "false");
-        }
     }
 
     themeButtons.forEach(button => {
