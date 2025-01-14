61c748ab0fe4fbe443e2f3c07c0d57626b87bbca
Selkiefawn <136149232+Selkiefawn@users.noreply.github.com>
2025-01-13 19:35:02 -0500

    fix shooting star img

--- neocities/paracosms/js/paracosms.js
+++ neocities/paracosms/js/paracosms.js
@@ -27,7 +27,7 @@ function createStars() {
 function createShootingStar() {
     const overlay = document.querySelector('.overlay');
     const shootingStar = document.createElement('img');
-    shootingStar.src = 'css/img/shootingstar.png';
+    shootingStar.src = '../css/img/shootingstar.png';
     shootingStar.classList.add('shootingstar');
 
     const startX = Math.random() * 70 - 10;
